676c5162a44ca99a1eb0a7323bbe088e3085edb1
ChovoITS <m.civita@itsrizzoli.it>
2023-01-25 22:54:26 +0100

    Adding writeToFile function
    This function create a new file if is not found else read the file and write a word in the textToWrite and close the file

--- src/main.py
+++ src/main.py
@@ -3,8 +3,22 @@ def fileOpen(filePath):
         lines = f.readlines()
     return lines
 
+def writeToFile(filePath, textToWrite):
+    try:
+        f = open(filePath, 'a')
+    except FileNotFoundError:
+        f = open(filePath, 'x')
+    except:
+        print("Error reading file")
+    else:
+        f = open(filePath, 'a')
+    finally:
+        f.write(textToWrite)
+        f.close()
+
 def main():
     linesProverb = fileOpen("proverbFile/proverb.txt")
+    writeToFile("proverbFile/file_proverb_even.txt", "Test")
 
 if __name__ == "__main__":
     main()
