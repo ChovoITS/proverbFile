5e476c476a21c68ab39442d849d9e2e20406c441
ChovoITS <m.civita@itsrizzoli.it>
2023-01-26 09:03:08 +0100

    Fix ads path

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 def fileOpen(filePath):
-    with open(os.path.join(sys.path[0], filePath), 'r') as f:
+    with open(os.path.join(os.path.abspath('./docs'), filePath), 'r') as f:
         lines = f.readlines()
     return lines
 
@@ -11,27 +11,27 @@ def fileOpen(filePath):
 def writeToFile(filePath, textToWrite):
     #Try except to prevent not existing files
     try:
-        f = open(os.path.join(sys.path[0], filePath), 'a')
+        f = open(os.path.join(os.path.abspath('./docs'), filePath), 'a')
     except FileNotFoundError:
-        f = open(os.path.join(sys.path[0], filePath), 'x') #File is not existing
+        f = open(os.path.join(os.path.abspath('./docs'), filePath), 'x') #File is not existing
     except:
         print("Error reading file") #Generic error
     else:
-        f = open(os.path.join(sys.path[0], filePath), 'a') #File is existing
+        f = open(os.path.join(os.path.abspath('./docs'), filePath), 'a') #File is existing
     finally:
         f.write(textToWrite)
         f.close()
     
 
 def main():
-    linesProverb = fileOpen("../docs/proverb.txt")
+    linesProverb = fileOpen("proverbFile.txt")
     for line in linesProverb:
         for charIndex in range(len(line)):
             if line[charIndex] != ' ':
                 if charIndex % 2 == 0: #Even
-                    writeToFile("../docs/file_proverb_even.txt", line[charIndex])
+                    writeToFile("file_proverb_even.txt", line[charIndex])
                 elif charIndex % 2 == 1:#Odd
-                    writeToFile("../docs/file_proverb_odd.txt", line[charIndex])
+                    writeToFile("file_proverb_odd.txt", line[charIndex])
 
 #This is a condition statement that that checks whether the value of the variable __name__ is equal to the string "__main__"
 #This is the case when you run your Python file as a script from the command line
